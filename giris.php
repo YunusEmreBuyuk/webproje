1ff451151f1c9c04c6a7266b76e6840e943b6d13
YunusEmreBuyuk <yunusbuyuk61@gmail.com>
2025-05-13 19:29:01 +0300

    webproje4

--- giris.php
+++ giris.php
@@ -2,16 +2,9 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
-    if (empty($username) || empty($password)) {
-        echo "Kullanıcı adı ve şifre boş bırakılamaz!";
-        exit;
-    }
-    if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
-        echo "Geçerli bir e-posta adresi giriniz!";
-        exit;
-    }
     if (!str_ends_with($username, "@sakarya.edu.tr")) {
         echo "Sadece @sakarya.edu.tr uzantılı e-posta adresleri kabul edilmektedir!";
+        header("Refresh: 1; url=proje.html");
         exit;
     }
     $student_number = explode("@", $username)[0];
